87e58dfcac16f4313e7a4a6a74f1a1499f11b158
YoussefTakali <yousseftakali159@gmail.com>
2025-06-22 19:12:08 +0100

    test

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://rapido-backend-0f4c0701c47b.herokuapp.com',
+  // apiBaseUrl: 'https://rapido-backend-0f4c0701c47b.herokuapp.com',
+  apiBaseUrl: 'localhost:3000', // Use this for local development
+
   api_key_heremaps: 'Fsnd_yfPDNmXB59WZi0z_7Cf8A48FGKjZ1_FpG_Q3Ok', // Replace with your actual HERE Maps API key
 };
